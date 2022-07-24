f4175da7930e63fbecc438916be0d62cd3a7099c
Amrit Pant <info.amritpant@gmail.com>
2022-07-24 19:54:05 +0545

    Added function for handling uncaughtException and unhandledRejection

--- server/server.js
+++ server/server.js
@@ -2,12 +2,31 @@ const path = require('path');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: path.join(__dirname, 'config', 'config.env') });
+
+//  Hadling unCaught Exception
+process.on('uncaughtException', err => {
+  console.log(`Error:  ${err.message}`);
+  console.error('Shutting down the server due to Uncaught Exception ');
+  process.exit(1);
+});
+
 const app = require('./app');
+const connectDatabase = require('./config/database');
 
 const { PORT } = process.env;
+connectDatabase();
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server has been started successfully on http://localhost:${PORT}`
   );
+});
+
+//  Hadling unCaught Exception
+process.on('unhandledRejection', err => {
+  console.log(`Error:  ${err.message}`);
+  console.error('Shutting down the server due ot unhanlded Rejection ');
+  server.close(() => {
+    process.exit(1);
+  });
 });
