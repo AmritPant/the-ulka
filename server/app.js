0d0109c5a5f86ca7826b0e7f226168116d9d067d
Amrit Pant <info.amritpant@gmail.com>
2022-07-24 19:58:31 +0545

    Added error Handler middleware in app.js

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 const morgan = require('morgan');
 const express = require('express');
 const productRouter = require('./routes/productRoute');
+const errorMiddleware = require('./middleware/error');
 
 const app = express();
 
-app.use(express.Router());
+app.use(express.json());
 app.use(morgan('dev'));
 
 // Routers
 app.use('/api/v1', productRouter);
+
+// Error Handling
+app.use(errorMiddleware);
 
 module.exports = app;
