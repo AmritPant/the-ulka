68e8fb0e68e9b7a50c5682d19724825bf1d05268
Amrit Pant <info.amritpant@gmail.com>
2022-07-24 20:02:48 +0545

    Added CRUD Operations Route

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const { getAllProducts } = require('../controllers/productController');
+const productController = require('../controllers/productController');
 
 const productRouter = express.Router();
 
-productRouter.route('/products').get(getAllProducts);
+productRouter.route('/products').get(productController.getAllProducts);
+
+productRouter.route('/product').post(productController.createNewProduct);
+
+productRouter
+  .route('/product/:id')
+  .get(productController.getOneProduct)
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
 
 module.exports = productRouter;
